982ecb703f2284dd5e53a9488cc2426a9edc22f9
ringyuki <hikarinagi@outlook.com>
2025-07-09 14:49:12 +0800

    fix: 更新 ecosystem.config.js 和 CI/CD 配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,13 @@ module.exports = {
   apps: [
     {
       name: 'hikarinagi-backend-nestjs',
-      script: 'pnpm',
-      args: 'start:prod',
+      script: 'dist/main.js',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
       env_file: '.env',
-      env: {
+      env_production: {
         NODE_ENV: 'production',
       },
     },
