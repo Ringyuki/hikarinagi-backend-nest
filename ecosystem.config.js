ed4d7fd1e84cfab80b56f0f17baa27d057bb757f
ringyuki <hikarinagi@outlook.com>
2025-07-09 14:30:13 +0800

    chore: 更新ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,17 +8,9 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
+      env_file: '.env',
       env: {
         NODE_ENV: 'production',
-        PORT: 3005,
-      },
-      env_staging: {
-        NODE_ENV: 'staging',
-        PORT: 3006,
-      },
-      env_production: {
-        NODE_ENV: 'production',
-        PORT: 3005,
       },
     },
   ],
