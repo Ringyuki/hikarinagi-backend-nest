857b55cfa4e4d5c3237dbe38381bad4fc0bc3da0
ringyuki <hikarinagi@outlook.com>
2025-08-04 12:14:12 +0800

    feat(main): disable x-powered-by res header

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,13 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter'
 import { EnvironmentValidator } from './common/config/validators/env.validator'
 import * as cookieParser from 'cookie-parser'
 import { VersionService } from './common/services/version.service'
+import { NestExpressApplication } from '@nestjs/platform-express'
 
 EnvironmentValidator.validateEnvironment()
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  app.disable('x-powered-by')
   const configService = app.get(ConfigService)
 
   // Cookie 解析器
